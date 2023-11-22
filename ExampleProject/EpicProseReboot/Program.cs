beca27d99ae9810bae15d0bab1457348664f6a2e
logicandchaos <logicandchaos1@gmail.com>
2023-11-22 13:18:08 -0500

    deleted Templates file from library, moved relavant code to game

--- ExampleProject/EpicProseReboot/Program.cs
+++ ExampleProject/EpicProseReboot/Program.cs
@@ -11,6 +11,13 @@ using TextAdventureLibrary;
 /// This is a remake of my Text Adventure Epic Prose to test out my TextAdventureLibrary
 /// </summary>
 
+//Predicate<Person> hasSwordEquiped = person => person.GetAttributeValue<Thing>("equipedItem") == sword;
+
+//PersonBuilder personBuilder = new PersonBuilder();
+//Person character = personBuilder
+//    .WithAttributes(human)
+//    .Build();
+
 namespace EpicProseRedux
 {
     class Program
@@ -59,11 +66,34 @@ namespace EpicProseRedux
         //king of thieves - human
         //static Template dragon = new Template("dragon");
 
-        //item templates
-        //items
-        //weapons
-        //armour
-        //sheilds
+        //name geberators
+        static NameGenerator humanMaleNameGenerator;
+        static NameGenerator humanFemaleNameGenerator;
+        static NameGenerator humanLastNameGenerator;
+        static NameGenerator animalNameGenerator;
+        static NameGenerator monsterNameGenerator;
+
+        //Species
+        public static Dictionary<string, object> human = new Dictionary<string, object>();
+        public static Dictionary<string, object> slime = new Dictionary<string, object>();
+        public static Dictionary<string, object> wolf = new Dictionary<string, object>();
+        public static Dictionary<string, object> bandit = new Dictionary<string, object>();//could use human?
+        public static Dictionary<string, object> briggand = new Dictionary<string, object>();
+        public static Dictionary<string, object> ogre = new Dictionary<string, object>();
+        public static Dictionary<string, object> goblin = new Dictionary<string, object>();
+        public static Dictionary<string, object> hobgoblin = new Dictionary<string, object>();
+        public static Dictionary<string, object> bugbear = new Dictionary<string, object>();
+        public static Dictionary<string, object> troll = new Dictionary<string, object>();
+        public static Dictionary<string, object> wyvern = new Dictionary<string, object>();
+        public static Dictionary<string, object> manticore = new Dictionary<string, object>();
+        public static Dictionary<string, object> skeleton = new Dictionary<string, object>();
+        public static Dictionary<string, object> skeletonWarrior = new Dictionary<string, object>();
+        public static Dictionary<string, object> mummy = new Dictionary<string, object>();
+        public static Dictionary<string, object> jackal = new Dictionary<string, object>();
+
+        public static PersonBuilder personBuilder = new PersonBuilder();
+        public static PlaceBuilder placeBuilder = new PlaceBuilder();
+        public static ThingBuilder thingBuilder = new ThingBuilder();
 
         static Person player;
 
@@ -134,9 +164,6 @@ namespace EpicProseRedux
                         //name
                         string name = console.GetString("Enter Your Name: ");
 
-                        //die
-                        player.die = new Die(name.GetHashCode(), 10);
-
                         //choose gender
                         string gender = "";
                         Menu selectGender = new Menu
@@ -152,6 +179,9 @@ namespace EpicProseRedux
                             .WithAttribute("name", name)
                             .WithAttribute("gender", gender)
                             .Build();
+
+                        //die
+                        player.die = new Die(name.GetHashCode(), 10);
 
                         console.Print("Roll Stats", true);
                         //roll stats
@@ -185,6 +215,7 @@ namespace EpicProseRedux
         public static void Setup()
         {
             SetupMenues();
+            SetupNameGenerators();
             SetupSpeciesTemplates();
             SetupItemTemplates();
             SetupPlaceTemplates();
@@ -290,8 +321,147 @@ namespace EpicProseRedux
 
         }
 
-        public static void SetupSpeciesTemplates()//?
+        public static void SetupNameGenerators()
         {
+            humanMaleNameGenerator = new NameGenerator
+                (
+                "Aelfric",
+                "Eadmund",
+                "Leofwine",
+                "Wulfstan",
+                "Godwin",
+                "Aldred",
+                "Cuthbert",
+                "Hrothgar",
+                "Ecgbert",
+                "Aethelred",
+                "Alfred",
+                "Cenric",
+                "Osgood",
+                "Sigurd",
+                "Guthrum",
+                "Hengest",
+                "Ceolwulf",
+                "Osric",
+                "Wilfrid",
+                "Ethelbert"
+                );
+
+            humanFemaleNameGenerator = new NameGenerator
+                (
+                "Aeliana",
+                "Aelis",
+                "Aldora",
+                "Aldwynn",
+                "Aveline",
+                "Berhtrude",
+                "Brunhilda",
+                "Edith",
+                "Elizabeth",
+                "Ethelwyn",
+                "Gisela",
+                "Grimilda",
+                "Guinevere",
+                "Hildeburg",
+                "Hildred",
+                "Hrothwyn",
+                "Mildrith",
+                "Thyra",
+                "Wynfrith",
+                "Zelda"
+                );
+
+            humanLastNameGenerator = new NameGenerator
+                (
+                "Glover",
+                "Osricson",
+                "Wynstan",
+                "Wilfridson",
+                "Eadmundson",
+                "Cuthbert",
+                "Fletcher",
+                "Miller",
+                "Taylor",
+                "Carpenter",
+                "Cooper",
+                "Baker",
+                "Fisher",
+                "Archer",
+                "Mason",
+                "Shepherd",
+                "Sexton",
+                "Tanner",
+                "Bowman",
+                "Cartwright"
+                );
+
+            animalNameGenerator = new NameGenerator
+                (
+                "Glover",
+                "Osricson",
+                "Wynstan",
+                "Wilfridson",
+                "Eadmundson",
+                "Cuthbert",
+                "Fletcher",
+                "Miller",
+                "Taylor",
+                "Carpenter",
+                "Cooper",
+                "Baker",
+                "Fisher",
+                "Archer",
+                "Mason",
+                "Shepherd",
+                "Sexton",
+                "Tanner",
+                "Bowman",
+                "Cartwright"
+                );
+
+            monsterNameGenerator = new NameGenerator
+                (
+                "Glover",
+                "Osricson",
+                "Wynstan",
+                "Wilfridson",
+                "Eadmundson",
+                "Cuthbert",
+                "Fletcher",
+                "Miller",
+                "Taylor",
+                "Carpenter",
+                "Cooper",
+                "Baker",
+                "Fisher",
+                "Archer",
+                "Mason",
+                "Shepherd",
+                "Sexton",
+                "Tanner",
+                "Bowman",
+                "Cartwright"
+                );
+        }
+
+        public static void SetupSpeciesTemplates()
+        {
+            //HUMAN
+            human.Add("species", "human");
+            human.Add("bodyType", "humanoid");
+            //stats
+            human.Add("strength", new Stat(3, 9));
+            human.Add("vitality", new Stat(3, 9));
+            human.Add("dexterity", new Stat(3, 9));
+            human.Add("speed", new Stat(3, 9));
+            human.Add("intelligence", new Stat(3, 9));
+            human.Add("charisma", new Stat(3, 9));
+            //utilities
+            Utility health = new Utility(10, 10);
+            human.Add("health", health);
+
+            //SWORD
+            Thing sword = new ThingBuilder().Build();
         }
 
         public static void SetupNPCs()
