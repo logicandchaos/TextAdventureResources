fd0db3349f0dd68952a463a224ecbe0f3e5dbb7b
logicandchaos <logicandchaos1@gmail.com>
2024-02-02 02:33:08 -0500

    I added save system to world with newtonsoft.JSon, I made history system part of world as well. I worked on and possibly finished Inventory System

--- TextAdventureLibrary/Inventory.cs
+++ TextAdventureLibrary/Inventory.cs
@@ -1,30 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// This is the Inventory class for managing the inventory.
+/// I made it a List rather than a dictionary, because I may need to add multiple of the same objets
+/// and having an order could be useful for iterating aand other things.
+/// I added in a weight and volume system if you want to limit either of these in your game.
+/// weight is in pounds and volume is in cubic inches.
+/// </summary>
 
 namespace TextAdventureLibrary
 {
     public class Inventory
     {
         public float MaxWeight { get; private set; }
-        public float Volume { get; private set; }
+        public float MaxVolume { get; private set; }
+        public float CurrentWeight { get; private set; }
+        public float VolumeUsed { get; private set; }
         public List<Thing> Things { get; private set; }
 
-        public Inventory(float maxWeight, float volume)
+        public Inventory(float maxWeight, float space)
         {
             MaxWeight = maxWeight;
-            Volume = volume;
+            MaxVolume = space;
+            CurrentWeight = 0;
+            VolumeUsed = 0;
         }
 
-        public void AddToInventory(Thing thing) { }
-
-        public void RemoveFromInventory(Thing thing) { }
-
-        public void RemoveFromInventory(int index) { }
-
-        public Thing SelectItem(int index)
+        public bool TryAddToInventory(Thing thing)
         {
-            return null;
+            float weight = thing.GetAttributeValue<float>("weight");
+            float volume = thing.GetAttributeValue<float>("volume");
+            if (weight + CurrentWeight <= MaxWeight
+                && volume + VolumeUsed <= MaxVolume)
+            {
+                Things.Add(thing);
+                CurrentWeight += weight;
+                VolumeUsed += volume;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryRemoveFromInventory(Thing thing)
+        {
+            if (Things.Contains(thing))
+            {
+                float weight = thing.GetAttributeValue<float>("weight");
+                float volume = thing.GetAttributeValue<float>("volume");
+                Things.Remove(thing);
+                CurrentWeight -= weight;
+                VolumeUsed -= volume;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryRemoveFromInventory(int index)
+        {
+            if (Things.Count > index)
+            {
+                float volume = Things[index].GetAttributeValue<float>("volume");
+                float weight = Things[index].GetAttributeValue<float>("weight");
+                Things.RemoveAt(index);
+                CurrentWeight -= weight;
+                VolumeUsed -= volume;
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- TextAdventureLibrary/World.cs
+++ TextAdventureLibrary/World.cs
@@ -1,36 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
+
+/// <summary>
+/// World class contains everything contained in the world. Lists for Everyone, Everywhere, Everything.
+/// </summary>
 
 namespace TextAdventureLibrary
 {
     public class World
     {
-        public string Name { get; private set; }
-        public Matrix Map { get; set; }
-        public Die die;
-        public DateTime DateTimeCurrent { get; private set; }
-        public Dictionary<string, Person> everyone;
-        public Dictionary<string, Place> everywhere;
-        public Dictionary<string, Thing> everything;
-        public Dictionary<DateTime, string> history;
-
+        static string saveFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SaveData");
         /*private static readonly Lazy<World> lazyInstance = new Lazy<World>(() => new World());
         public static World Instance => lazyInstance.Value;
         private World() { }*/
 
-        public World() { }
+        public string Name { get; private set; }
+        public Matrix Map { get; set; }
+        public Die die;
+        public DateTime DateTimeCurrent { get; private set; }
+        public List<Person> Everyone { get; set; }
+        public List<Place> Everywhere { get; set; }
+        public List<Thing> Everything { get; set; }
+        public Dictionary<DateTime, string> History { get; private set; }
 
         public World(string name)
         {
+            Name = name;
             //create die
             die = new Die(name.GetHashCode());
-        }
-
-        public World(int seed)
-        {
-            //create die
-            die = new Die(seed);
+            Everyone = new List<Person>();
+            Everywhere = new List<Place>();
+            Everything = new List<Thing>();
+            History = new Dictionary<DateTime, string>();
         }
 
         public void AddTimeSpan(TimeSpan timeSpan)
@@ -38,16 +43,50 @@ namespace TextAdventureLibrary
             DateTimeCurrent += timeSpan;
         }
 
-        public void Save()
+        public void AddHistoricalEvent(DateTime dateTime, string historicalEvent)
         {
-            //serialize all the data
-            //save to text file
+            if (History.ContainsKey(dateTime))
+            {
+                History.Add(dateTime, History.TryGetValue(dateTime, out string value) + "\n" + historicalEvent);
+            }
+            else
+                History.Add(dateTime, historicalEvent);
         }
 
-        public void Load(string worldName)
+        public List<string> GetEventsAbout(string subject)
         {
-            //read in the textfile for the worldName
-            //parse string data into world
+            return History.Where(entry => entry.Value.Contains(subject))
+                          .Select(entry => $"{entry.Key.ToShortDateString()}: {entry.Value}")
+                          .ToList();
+        }
+
+        public void Save()
+        {
+            if (!Directory.Exists(saveFolderPath))
+            {
+                Directory.CreateDirectory(saveFolderPath);
+            }
+
+            string fileName = $"{Name}.json";
+            string filePath = Path.Combine(saveFolderPath, fileName);
+
+            // Serialize all data
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+            // Write JSON to file
+            File.WriteAllText(filePath, json);
+        }
+
+        public static World Load(string worldName)
+        {
+            string fileName = $"{worldName}.json";
+            string filePath = Path.Combine(saveFolderPath, fileName);
+
+            // Read JSON from file
+            string json = File.ReadAllText(filePath);
+
+            // Deserialize JSON to object
+            return JsonConvert.DeserializeObject<World>(json);
         }
     }
 }
