788d87e1981ed2545a0f02a396772e582c995692
logicandchaos <logicandchaos1@gmail.com>
2024-02-03 02:33:28 -0500

    worked on multimenu builder, removed children of place, added connected places to place

--- TextAdventureLibrary/MultiMenuBuilder.cs
+++ TextAdventureLibrary/MultiMenuBuilder.cs
@@ -10,21 +10,70 @@ namespace TextAdventureLibrary
 {
     public class MultiMenuBuilder
     {
-        List<Menu> menus;
+        string name;
+        List<MenuItem> menuItems;
+
+        public MultiMenuBuilder WithName(string name)
+        {
+            this.name = name;
+            return this;
+        }
 
         public MultiMenuBuilder WithItem(MenuItem menuItem)
         {
+            menuItems.Add(menuItem);
             return this;
         }
 
-        public MultiMenuBuilder WithItems(params MenuItem[] menuItem)
+        public MultiMenuBuilder WithItems(params MenuItem[] menuItems)
         {
+            this.menuItems.AddRange(menuItems);
             return this;
         }
+
+        /*public Menu Build()
+        {
+            if (menuItems.Count > 10)
+                return null;
+
+            Menu menu = new Menu(name);
+            menu.items = menuItems.ToArray();
+            return menu;
+        }*/
 
         public List<Menu> Build()
         {
-            //this is where the menu items will be split into different menus
+            List<Menu> menus = new List<Menu>();
+
+            if (menuItems.Count > 10)
+            {
+                int numMenus = menuItems.Count / 8;
+                if (menuItems.Count % 8 != 0)
+                    numMenus++;
+
+                List<MenuItem> items;
+
+                for (int i = 0; i < numMenus; i++)
+                {
+                    items = new List<MenuItem>();
+                    menus.Add(new Menu(name));
+                    for (int j = 0; j < 8; j++)
+                    {
+                        items.Add(menuItems[i + j]);
+                    }
+                    //TODO
+                    //need to add proper Action to link the menues together
+                    items.Add(new MenuItem("prev", () => { }));
+                    items.Add(new MenuItem("next", () => { }));
+                    menus[i].items = items.ToArray();
+                }
+            }
+            else
+            {
+                menus[0] = new Menu(name);
+                menus[0].items = menuItems.ToArray();
+            }
+
             return menus;
         }
     }
--- TextAdventureLibrary/Place.cs
+++ TextAdventureLibrary/Place.cs
@@ -1,7 +1,16 @@
-﻿namespace TextAdventureLibrary
+﻿using System.Collections.Generic;
+
+namespace TextAdventureLibrary
 {
     public class Place : Noun
     {
+        public Dictionary<string, Place> connected;
+
         public Place() { }
+
+        public List<string> GetConnected()
+        {
+            return new List<string>(connected.Keys);
+        }
     }
 }
