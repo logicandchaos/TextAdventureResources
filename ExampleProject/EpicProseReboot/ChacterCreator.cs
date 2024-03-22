a816c536b3bad17f1999fd78ce7edb4babf1e29e
logicandchaos <logicandchaos1@gmail.com>
2024-03-22 03:06:12 -0400

    moved methods from Game to CharacterCreator

--- ExampleProject/EpicProseReboot/ChacterCreator.cs
+++ ExampleProject/EpicProseReboot/ChacterCreator.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TextAdventureLibrary;
 
 namespace EpicProseRedux
@@ -25,10 +22,137 @@ namespace EpicProseRedux
             "diet"
             );
 
+        public static NameGenerator humanMaleNameGenerator = new NameGenerator
+                (
+                "Aelfric",
+                "Eadmund",
+                "Leofwine",
+                "Wulfstan",
+                "Godwin",
+                "Aldred",
+                "Cuthbert",
+                "Hrothgar",
+                "Ecgbert",
+                "Aethelred",
+                "Alfred",
+                "Cenric",
+                "Osgood",
+                "Sigurd",
+                "Guthrum",
+                "Hengest",
+                "Ceolwulf",
+                "Osric",
+                "Wilfrid",
+                "Ethelbert"
+                );
+
+        public static NameGenerator humanFemaleNameGenerator = new NameGenerator
+            (
+                "Aeliana",
+                "Aelis",
+                "Aldora",
+                "Aldwynn",
+                "Aveline",
+                "Berhtrude",
+                "Brunhilda",
+                "Edith",
+                "Elizabeth",
+                "Ethelwyn",
+                "Gisela",
+                "Grimilda",
+                "Guinevere",
+                "Hildeburg",
+                "Hildred",
+                "Hrothwyn",
+                "Mildrith",
+                "Thyra",
+                "Wynfrith",
+                "Zelda"
+                );
+
+        public static NameGenerator humanLastNameGenerator = new NameGenerator
+            (
+                "Glover",
+                "Osricson",
+                "Wynstan",
+                "Wilfridson",
+                "Eadmundson",
+                "Cuthbert",
+                "Fletcher",
+                "Miller",
+                "Taylor",
+                "Carpenter",
+                "Cooper",
+                "Baker",
+                "Fisher",
+                "Archer",
+                "Mason",
+                "Shepherd",
+                "Sexton",
+                "Tanner",
+                "Bowman",
+                "Cartwright"
+                );
+
+        public static NameGenerator animalNameGenerator = new NameGenerator
+            (
+                "Glover",
+                "Osricson",
+                "Wynstan",
+                "Wilfridson",
+                "Eadmundson",
+                "Cuthbert",
+                "Fletcher",
+                "Miller",
+                "Taylor",
+                "Carpenter",
+                "Cooper",
+                "Baker",
+                "Fisher",
+                "Archer",
+                "Mason",
+                "Shepherd",
+                "Sexton",
+                "Tanner",
+                "Bowman",
+                "Cartwright"
+                );
+
+        public static NameGenerator monsterNameGenerator = new NameGenerator
+            (
+                "Glover",
+                "Osricson",
+                "Wynstan",
+                "Wilfridson",
+                "Eadmundson",
+                "Cuthbert",
+                "Fletcher",
+                "Miller",
+                "Taylor",
+                "Carpenter",
+                "Cooper",
+                "Baker",
+                "Fisher",
+                "Archer",
+                "Mason",
+                "Shepherd",
+                "Sexton",
+                "Tanner",
+                "Bowman",
+                "Cartwright"
+                );
+
         public static void RollStats(Person person)
         {
             Die die = person.GetAttributeValue<Die>("die");
+
             Dictionary<string, Stat> stats = person.FilterAttributesByType<Stat>();
+            // Use LINQ to filter objects of the specified type
+            /*List<Stat> stats = person.Attributes
+                .Where(kv => kv.Value != null && kv.Value.GetType() == type)
+                .Select(kv => (Stat)kv.Value)
+                .ToList();*/
+
             foreach (var stat in stats)
             {
                 stat.Value.RollStat(die);
